beb825ebca14e6a4106ec54c9d65683856d547f5
Allumodex <spm11347@gmail.com>
2024-09-20 10:29:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,20 +4,20 @@
 //Instagram: techgod143
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: +919746374936
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
+global.ownernomer = "919746374936"
 global.ownername = "🐛Tech God"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '919746374936'  //creator number
+global.ownername = '🐛𝜧𝝈𝝒𝝑𝛂𝛞 𝜦𝝇𝝅𝝊➤' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
